9b11db3a94b9447897d54e67104cfd474ce8f6e6
geloodev <geloodev@gmail.com>
2023-10-06 16:55:09 -0300

    :sparkles: feat: character class, equipment and power classes

---
+++ src/main/java/com/geloodev/rpgcharactersheet/models/CharacterClass.java
@@ -0,0 +1,8 @@
+package com.geloodev.rpgcharactersheet.models;
+
+import lombok.Data;
+
+@Data
+public class CharacterClass {
+    private String name;
+}
---
+++ src/main/java/com/geloodev/rpgcharactersheet/models/CharacterEquipment.java
@@ -0,0 +1,8 @@
+package com.geloodev.rpgcharactersheet.models;
+
+import lombok.Data;
+
+@Data
+public class CharacterEquipment {
+    private String name;
+}
