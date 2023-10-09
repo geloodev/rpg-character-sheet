9644f4e1dda3a40482f87f845811dd939a30dfe2
geloodev <geloodev@gmail.com>
2023-10-09 16:23:36 -0300

    :recycle: refactor: changing getUsers() to return users view

--- src/main/java/com/geloodev/rpgcharactersheet/controllers/UserControler.java
+++ src/main/java/com/geloodev/rpgcharactersheet/controllers/UserControler.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.geloodev.rpgcharactersheet.models.User;
 import com.geloodev.rpgcharactersheet.models.Character;
@@ -31,8 +32,11 @@ public class UserControler {
     }
 
     @GetMapping
-    public List<User> getUsers() {
-        return userRepository.findAll();
+    public ModelAndView getUsers() {
+        List<User> users = userRepository.findAll();
+        ModelAndView mv = new ModelAndView("users");
+        mv.addObject("users", users);
+        return mv;
     }
 
     @GetMapping("/{id}")
