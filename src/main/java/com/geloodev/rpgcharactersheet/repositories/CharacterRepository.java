1a094b9f4768ed1ab1bd2ffceff720652d8d03c8
geloodev <geloodev@gmail.com>
2023-10-16 16:14:42 -0300

    :sparkles: feat: findByUserId method

--- src/main/java/com/geloodev/rpgcharactersheet/repositories/CharacterRepository.java
+++ src/main/java/com/geloodev/rpgcharactersheet/repositories/CharacterRepository.java
@@ -1,7 +1,12 @@
 package com.geloodev.rpgcharactersheet.repositories;
 
+import java.util.Collection;
+
+import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.geloodev.rpgcharactersheet.models.Character;
 
-public interface CharacterRepository extends MongoRepository<Character, String> {}
+public interface CharacterRepository extends MongoRepository<Character, String> {
+    Collection<Character> findByUserId(ObjectId userId);
+}
