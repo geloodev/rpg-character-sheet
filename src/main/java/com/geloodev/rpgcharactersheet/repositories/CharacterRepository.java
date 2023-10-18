f6d8683b20b7ee8208c69491f06572b23f99ca00
geloodev <geloodev@gmail.com>
2023-10-18 17:39:50 -0300

    :recycle: refactor: changing findById method to findByUsername

--- src/main/java/com/geloodev/rpgcharactersheet/repositories/CharacterRepository.java
+++ src/main/java/com/geloodev/rpgcharactersheet/repositories/CharacterRepository.java
@@ -2,11 +2,10 @@ package com.geloodev.rpgcharactersheet.repositories;
 
 import java.util.Collection;
 
-import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.geloodev.rpgcharactersheet.models.Character;
 
 public interface CharacterRepository extends MongoRepository<Character, String> {
-    Collection<Character> findByUserId(ObjectId userId);
+    Collection<Character> findByUsername(String username);
 }
