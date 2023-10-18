8392bb44e2f960e769dd486aa1f717bd07b9967b
geloodev <geloodev@gmail.com>
2023-10-18 17:38:41 -0300

    :sparkles: feat: findByUsername method

--- src/main/java/com/geloodev/rpgcharactersheet/repositories/UserRepository.java
+++ src/main/java/com/geloodev/rpgcharactersheet/repositories/UserRepository.java
@@ -4,4 +4,6 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.geloodev.rpgcharactersheet.models.User;
 
-public interface UserRepository extends MongoRepository<User, String> {}
+public interface UserRepository extends MongoRepository<User, String> {
+    User findByUsername(String username);
+}
